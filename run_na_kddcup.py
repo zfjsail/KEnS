eb51422447c58f6d4347a20d8bf22176847778f0
Fanjin Zhang <zfjsail@163.com>
2021-10-28 15:09:26 +0800

    train kens kddcup

--- run_na_kddcup.py
+++ run_na_kddcup.py
@@ -1,37 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-"""
-Working directory: project root
-
-In all variable names, 0 denotes the target/sparse kg and 1 denotes the source/dense kg.
-"""
+import argparse
 import src.param as param
-
 import os
 from os.path import join
-
-print('Current working dir', os.getcwd())
-import sys
-
-if './' not in sys.path:
-    sys.path.append('./')
-
-from numpy.linalg import norm
+import numpy as np
 import pandas as pd
-from sklearn.metrics import average_precision_score
-import src.param as param
-from src.model import save_model_structure
+import logging
 from src.data_loader import load_support_kgs, load_target_kg, load_all_to_all_seed_align_links
 from src.validate import MultiModelTester, TestMode, test_alignment_hits10
-import numpy as np
-import logging
-from sklearn.metrics import roc_auc_score
-from collections import defaultdict as dd
-from src.model import extend_seed_align_links
-import argparse
-import utils
+from src.model import save_model_structure
 
 
 def set_logger(param, model_dir):
@@ -60,7 +36,7 @@ def parse_args(args=None):
         description='Training and Testing Knowledge Graph Embedding Models',
         usage='run.py [<args>] [-h | --help]'
     )
-    parser.add_argument('-l', '--target_language', default="aminer", type=str, choices=['ja', 'el', 'es', 'fr', 'en'],
+    parser.add_argument('-l', '--target_language', default="mag", type=str, choices=['ja', 'el', 'es', 'fr', 'en'],
                         help="target kg")
     parser.add_argument('-m', '--knowledge_model', default='transe', type=str, choices=['transe', 'rotate'])
     parser.add_argument('--use_default', action="store_true",
@@ -114,6 +90,7 @@ def set_params(args):
         param.round = args.round
 
 
+
 def main(args):
     set_params(args)
 
@@ -125,8 +102,8 @@ def main(args):
     target_lang = param.lang
 
     # load data
-    data_dir = '/home/zfj/research-data/na-checking/kens-na-data/kg'  # where you put kg data
-    seed_dir = '/home/zfj/research-data/na-checking/kens-na-data/seed_alignlinks'  # where you put seed align links data
+    data_dir = '/home/zfj/research-data/na-checking/kens-na-data-kddcup/kg'  # where you put kg data
+    seed_dir = '/home/zfj/research-data/na-checking/kens-na-data-kddcup/seed_alignlinks'  # where you put seed align links data
     model_dir = join('./trained_model', f'kens-{param.knowledge}-{param.dim}', target_lang)  # output
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
@@ -145,12 +122,6 @@ def main(args):
     # supporter kgs
     supporter_kgs = load_support_kgs(data_dir, seed_dir, target_lang, src_langs)  # list[KnowledgeGraph]
     # supporter KG use all links to train
-    for kg1 in supporter_kgs:
-        # kg1.h_train = np.concatenate([kg1.h_train, kg1.h_test], axis=0)
-        # kg1.r_train = np.concatenate([kg1.r_train, kg1.r_test], axis=0)
-        # kg1.t_train = np.concatenate([kg1.t_train, kg1.t_test], axis=0)
-        # kg1.y_train = np.zeros(kg1.h_train.shape[0])
-        pass
 
     # seed alignment links
     seed_alignlinks = load_all_to_all_seed_align_links(seed_dir)  # {(lang1, lang2): 2-col np.array}
@@ -180,19 +151,6 @@ def main(args):
 
         print("\n\n")
         logging.info("**********************************")
-        # self-learning
-        """
-        for kg in all_kgs:
-            for other_kg in all_kgs:
-                if other_kg.lang != kg.lang and (other_kg.lang, kg.lang) in seed_alignlinks:
-                    print(f'self learning[{kg.lang}][{other_kg.lang}]')
-                    logging.info("logging for time")
-                    seeds = seed_alignlinks[(other_kg.lang, kg.lang)]
-                    found = extend_seed_align_links(other_kg, kg, seeds)
-                    if len(found) > 0:  # not []
-                        new_seeds = np.concatenate([seeds, found], axis=0)
-                        seed_alignlinks[(other_kg.lang, kg.lang)] = new_seeds
-        """
 
         # train knowledge model
         logging.info("training target kg model")
@@ -225,8 +183,6 @@ def main(args):
     for kg1 in supporter_kgs:
         kg1.filtered_reordered_embedding_matrix = None
 
-    val_df = None
-    test_df = None
 
     for suffix in choices:
         testfile = join(data_dir, target_lang + suffix)
@@ -243,103 +199,6 @@ def main(args):
                 wf.write(str(d) + "\n")
 
 
-def eval_test_na():
-    file_dir = "/home/zfj/research-data/na-checking/aminer-new1"
-    pairs_test = utils.load_json(file_dir, "eval_na_checking_pairs_conna_filter_test.json")
-
-    model_dir = join('./trained_model', 'kens-transe-400', "aminer")  # output
-    dists = []
-    with open(join(model_dir, "results-dist-test.tsv")) as rf:
-        for line in rf:
-            d = float(line.strip())
-            d = 1 / (1 + np.exp(-d))
-            dists.append(d)
-
-    labels = []
-    for pair in pairs_test:
-        cur_label = pair["label"]
-        if cur_label == 1:
-            labels.append(0)
-        elif cur_label == 0:
-            labels.append(1)
-        else:
-            raise
-
-    auc = roc_auc_score(labels, dists)
-    print("kens auc", auc)
-    maps = average_precision_score(labels, dists)
-    print("kens map", maps)
-
-
-def eval_test_na_2():
-    file_dir = "/home/zfj/research-data/na-checking/aminer-new1"
-    pairs_test = utils.load_json(file_dir, "eval_na_checking_pairs_conna_filter_test.json")
-
-    model_dir = join('./trained_model', 'kens-transe-400', "aminer")  # output
-    dists = []
-    with open(join(model_dir, "results-dist-test.tsv")) as rf:
-        for line in rf:
-            d = float(line.strip())
-            d = 1 / (1 + np.exp(-d))
-            dists.append(d)
-
-    pairs_test_new = utils.load_json(file_dir, "eval_na_checking_triplets_test_idx_clean.json")
-    aid_to_test_pids_new = dd(set)
-    for pair in pairs_test_new:
-        aid_to_test_pids_new[pair["aid1"]].add(pair["pid"].split("-")[0])
-
-    aid_to_label = dd(list)
-    aid_to_score = dd(list)
-    for i, pair in enumerate(pairs_test):
-        pid = pair["pid"]
-        aid = pair["aid1"]
-        if str(pid) in aid_to_test_pids_new.get(aid, set()):
-            aid_to_label[aid].append(1-pair["label"])
-            aid_to_score[aid].append(dists[i])
-
-    map_sum = 0
-    map_weight = 0
-    auc_sum = 0
-    n_authors = 0
-    for aid in aid_to_label:
-        cur_n_pids = len(aid_to_label[aid])
-        cur_labels = aid_to_label[aid]
-        if sum(cur_labels)/len(cur_labels) >= 0.5 or sum(cur_labels) == 0 or cur_n_pids < 5:
-            continue
-        n_authors += 1
-        cur_map = average_precision_score(aid_to_label[aid], aid_to_score[aid])
-        map_sum += cur_map/len(aid_to_label[aid])
-        map_weight += 1/ len(aid_to_label[aid])
-        auc_sum += roc_auc_score(aid_to_label[aid], aid_to_score[aid])
-    map_avg = map_sum/map_weight
-    auc_avg = auc_sum / n_authors
-    print("auc avg", auc_avg, "map avg", map_avg)
-
-
-def test_eval(args):
-    set_params(args)
-    target_lang = param.lang
-    src_langs = ['aminer', 'mag']
-    src_langs.remove(target_lang)
-    data_dir = '/home/zfj/research-data/na-checking/kens-na-data/kg'  # where you put kg data
-    seed_dir = '/home/zfj/research-data/na-checking/kens-na-data/seed_alignlinks'  # where you put seed align links data
-    model_dir = join('./trained_model', f'kens-{param.knowledge}-{param.dim}', target_lang)  # output
-
-    kg0 = load_target_kg(data_dir, target_lang, testfile_suffix='-val.tsv')  # target kg. KnowledgeGraph object
-    supporter_kgs = load_support_kgs(data_dir, seed_dir, target_lang, src_langs)  # list[KnowledgeGraph]
-    validator = MultiModelTester(kg0, supporter_kgs)
-    suffix = "-test.tsv"
-    output = join(model_dir, 'results' + suffix)
-    testfile = join(data_dir, target_lang + suffix)
-    testcases = pd.read_csv(testfile, sep='\t', header=None).values
-    param.n_test = testcases.shape[0]  # test on all training triples
-    print('Loaded test cases from: %s' % testfile)
-
-    results_df = validator.calc_test_cases_sim(testcases)
-
-
 if __name__ == "__main__":
     main(parse_args())
-    # test_eval(parse_args())
-    # eval_test_na()
-    eval_test_na_2()
+    
