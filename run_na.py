aad6ca9f0d755ab1ea1fb00e4e04b3a934000312
Fanjin Zhang <zfjsail@163.com>
2022-05-24 21:12:00 +0800

    a

--- run_na.py
+++ run_na.py
@@ -345,7 +345,8 @@ def test_eval(args):
 
 
 if __name__ == "__main__":
-    # main(parse_args())
+    main(parse_args())
     # test_eval(parse_args())
     # eval_test_na()
+
     eval_test_na_2()
